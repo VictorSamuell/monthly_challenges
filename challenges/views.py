2bc3250382a8d4fb2153e4947c22e7fae0e84775
VictorSamuell <victor.samuel1@aluno.ifsp.edu.br>
2025-09-11 23:00:33 -0300

    Lista de meses e interatividade nas views

--- challenges/views.py
+++ challenges/views.py
@@ -17,6 +17,19 @@ monthly_challenges = {
     "december": "Bem-vindo a Dezembro",
 }
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    
+    
+    response_data = f"<ul>{list_items}</ul>"  
+    return HttpResponse(response_data)
+
 
 def monthly_challenge_by_number(request, month):
 
@@ -27,13 +40,15 @@ def monthly_challenge_by_number(request, month):
     
     redirect_month = months[month - 1]
     redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/january
-    return HttpResponseRedirect("/challenges/"+ redirect_month)
+    return HttpResponseRedirect(redirect_path)
     
 
 def monthly_challenge(request, month):
     try:
+        
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("Mês Inválido")
+        return HttpResponseNotFound("<h1> Mês Inválido </h1>")
     
